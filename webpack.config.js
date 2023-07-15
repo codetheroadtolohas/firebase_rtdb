6d9941c964d1982a35e526e90d3f3f65688c3ea1
Mona Lisa <monalisa@monalisa.com>
2023-07-15 16:05:03 +0800

    Add webpack config.
    Modify .gitignore to exlcude public/ folder.

---
+++ webpack.config.js
@@ -0,0 +1,24 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+  mode: "development",
+  entry: path.resolve(__dirname, "src/app.js"),
+  output: {
+    path: path.resolve(__dirname, "public"),
+    filename: "bundle.js",
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ["style-loader", "css-loader"],
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: path.resolve(__dirname, "src/index.html"),
+    }),
+  ],
+};
