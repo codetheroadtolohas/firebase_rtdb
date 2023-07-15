d325267c6f70f6f5eda8476f5beaada4ccf32e4a
Mona Lisa <monalisa@monalisa.com>
2023-07-15 16:47:47 +0800

    Connect to realtime db
    Create a node in realtime db

---
+++ src/app.js
@@ -0,0 +1,17 @@
+import { initializeApp } from "firebase/app";
+import { getDatabase, ref, push } from "firebase/database";
+
+const firebase = initializeApp({
+  apiKey: "AIzaSyAC-MrvYQrVekLmIypYa1z4rjYq0aQe-HA",
+  authDomain: "realtime-db-4615e.firebaseapp.com",
+  databaseURL:
+    "https://realtime-db-4615e-default-rtdb.asia-southeast1.firebasedatabase.app",
+  projectId: "realtime-db-4615e",
+  storageBucket: "realtime-db-4615e.appspot.com",
+  messagingSenderId: "242795240849",
+  appId: "1:242795240849:web:076dd4c6986ceb9dd5ce30",
+});
+
+const db = getDatabase(firebase);
+const users = ref(db, "users");
+push(users, "Jeff");
